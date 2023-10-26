b5f6300979ceb9b7dad7bb50f9984b269be93976
Julia Michel T. <juliamicheltex@gmail.com>
2023-10-26 21:26:04 +0200

    new calculator

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const buttons = document.querySelectorAll(".custom-btn");
+const output = document.getElementById("output2");
+let currentInput = "";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let lastButton = null;
 
-setupCounter(document.querySelector('#counter'))
+buttons.forEach((button, index) => {
+  button.addEventListener("click", function () {
+    const buttonText = this.textContent;
+
+    if (buttonText === "=") {
+      // Si le bouton "=" est cliqué, effectuez le calcul
+      output.value = eval(currentInput);
+      currentInput = "";
+    } else if (buttonText === "C") {
+      // Si le bouton "C" est cliqué, effacez l'input
+      currentInput = "";
+      output.value = "";
+    } else {
+      // Sinon, ajoutez le texte du bouton à l'input en cours
+      currentInput += buttonText;
+      output.value = currentInput;
+    }
+
+    // DEPLACE LE BOUTTON
+    button.style.transform = `translateX(${Math.random() * 120 - 60}px)`;
+
+    // REVIENT BOUTON
+    if (lastButton && lastButton !== button) {
+      lastButton.style.transform = "translateX(0)";
+    }
+
+    // Mémorisez le dernier bouton cliqué
+    lastButton = button;
+  });
+});
